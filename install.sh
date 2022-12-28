0d704a07b7fe13e665cb4be86af21df5cdd0e783
QuetzalCtrl <hugo.vanrobaeys@gmail.com>
2022-12-29 00:35:11 +0700

    ajout de install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# copy the quetzal script to the /usr/bin/ directory
+sudo cp quetzal /usr/bin/
+# make the quetzal script executable
+sudo chmod +x /usr/bin/quetzal
+
+echo "Installation complete"
